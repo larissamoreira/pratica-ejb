0ca85e1e5d7f27550a9e84d3f9185e654cb3164d
Larissa Gusmao <larissamgusmao@gmail.com>
2020-10-05 21:15:28 -0300

    Add messages

---
+++ src/main/java/com/gugawag/pdist/ejbs/MensagemDAO.java
@@ -0,0 +1,29 @@
+package com.gugawag.pdist.ejbs;
+
+import com.gugawag.pdist.model.Mensagem;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class MensagemDAO {
+
+    @PersistenceContext(unitName="default")
+    private EntityManager em;
+
+    public void inserir(Mensagem novaMsg){
+        em.persist(novaMsg);
+    }
+
+    public List<Mensagem> listar() {
+        return em.createQuery("FROM Mensagem").getResultList();
+    }
+
+    public List<Mensagem> pesquisarPorId(long id) {
+        return em.createQuery("SELECT m FROM Mensagem m WHERE m.id = :idMsg")
+                .setParameter("idMsg", id)
+                .getResultList();
+    }
+}
